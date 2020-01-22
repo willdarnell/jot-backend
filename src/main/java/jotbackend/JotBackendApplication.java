565b7233a1ef43d40d10ad53b855a79356ef5044
Will <willdarnell@gmail.com>
2020-01-22 16:28:24 -0600

    Initial commit

---
+++ src/main/java/jotbackend/JotBackendApplication.java
@@ -0,0 +1,13 @@
+package jotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jotbackend/JotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
