68b6596f7a8097eda326fd664b49b35de8ca686e
ailishb <37984176+ailishb@users.noreply.github.com>
2020-02-21 19:35:21 -0500

    Implemented updateAttribute

--- src/main/java/jotbackend/controllers/AttributeController.java
+++ src/main/java/jotbackend/controllers/AttributeController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -46,6 +47,21 @@ public class AttributeController {
     public @ResponseBody
     Optional<Attribute> getAttributeById(@PathVariable Integer attributeId) {
         return attributeRepository.findById(attributeId);
+    }
+
+    @PutMapping(path = "/update/{attributeId}")
+    public @ResponseBody String updateAttribute(@PathVariable Integer attributeId,
+                                              @RequestParam Integer userId,
+                                              @RequestParam String title,
+                                              @RequestParam String description){
+        Attribute updatedAttribute = attributeRepository.findById(attributeId)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        updatedAttribute.setUserId(userId);
+        updatedAttribute.setTitle(title);
+        updatedAttribute.setDescription(description);
+        attributeRepository.save(updatedAttribute);
+        return "Saved";
+
     }
 
 }
