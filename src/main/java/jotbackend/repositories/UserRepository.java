97143146e1b16a6582f67424af721121978d03d2
Will <willdarnell@gmail.com>
2020-02-24 15:40:49 -0600

    add delete user and update user

--- src/main/java/jotbackend/repositories/UserRepository.java
+++ src/main/java/jotbackend/repositories/UserRepository.java
@@ -1,10 +1,15 @@
 package jotbackend.repositories;
 
 import jotbackend.classes.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByGid(String gid);
+
+
+
 }
